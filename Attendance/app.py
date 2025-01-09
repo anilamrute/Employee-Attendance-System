dc12f3f29ff0c74ea83f1c7f24978d003194ed3d
Anil Amrute <60308655+anilamrute@users.noreply.github.com>
2025-01-09 15:46:05 +0530

    Update app.py

--- Attendance/app.py
+++ Attendance/app.py
@@ -154,4 +154,4 @@ def export_attendance():
     return send_file(export_file, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
